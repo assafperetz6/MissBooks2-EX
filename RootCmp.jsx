c46b35323d6210d4f8439818aaeb14864b2bc060
assafperetz6 <assafperetz6@gmail.com>
2024-09-18 22:12:26 +0300

    Add cmps and their conditional rendering

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,13 +1,31 @@
 import { Home } from './cmps/Home.jsx'
+import { AboutUs } from './cmps/AboutUs.jsx'
+import { BookIndex } from './cmps/BookIndex.jsx'
+
+const { useState } = React
 
 export function App() {
+    const [page, setPage] = useState('home')
+
+    function selectPage(pageName) {
+        setPage(pageName)
+    }
+
     return (
         <section className="app">
             <header className="app-header">
                 <h1>My App</h1>
+                <ul className="main-nav clean-list flex">
+                    <li onClick={() => selectPage('home')}>Home</li>
+                    <li onClick={() => selectPage('about')}>About Us</li>
+                    <li onClick={() => selectPage('book')}>Books</li>
+                </ul>
+
             </header>
             <main className="container">
-                <Home />
+                { page === 'home' && <Home />}
+                { page === 'about' && <AboutUs />}
+                { page === 'book' && <BookIndex />}
             </main>
         </section>
     )
--- cmps/Home.jsx
+++ cmps/Home.jsx
@@ -1,11 +1,7 @@
-import { AboutUs } from './AboutUs.jsx'
-import { BookIndex } from './BookIndex.jsx'
-
 export function Home() {
     return (
         <section>
             <h2>Home Sweet Home</h2>
-            <AboutUs />
         </section>
     )
 }
