d0a912f1ac4f14d3470df53b886bac380db4b036
assafperetz6 <assafperetz6@gmail.com>
2024-09-19 12:28:20 +0300

    Add styling for book details

--- cmps/BookDetails.jsx
+++ cmps/BookDetails.jsx
@@ -17,6 +17,13 @@ export function BookDetails({ book }) {
             <h2>{title}</h2>
             <img src={thumbnail} alt="book-img"/>
             <p>{description}</p>
+            <ul>
+                <li><span className="bold">Price:</span> {listPrice.amount} {listPrice.currencyCode}</li>
+                <li><span className="bold">Written by:</span> {authors.join(', ')}</li>
+                <li><span className="bold">Published:</span> {publishedDate}</li>
+                <li><span className="bold">Page count:</span> {pageCount}</li>
+                <li><span className="bold">Categories:</span> {categories.join(', ')}</li>
+            </ul>
         </article>
     )
 }
