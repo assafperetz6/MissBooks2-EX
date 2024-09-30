20c78c933f14e735ae2a4d2a2b6b8d2b622aaa95
assafperetz6 <assafperetz6@gmail.com>
2024-09-30 11:53:28 +0300

    Add setNextPrevBook fn

--- services/book.service.js
+++ services/book.service.js
@@ -5,76 +5,85 @@ import { storageService } from './async-storage.service.js'
 const BOOK_KEY = 'BOOK_DB'
 _createBooks()
 
-
-
-
 export const bookService = {
-    query,
-    get,
-    remove,
-    save,
-    getNewBook,
-    getNextbookId,
-    getDefaultFilter
+	query,
+	get,
+	remove,
+	save,
+	getNewBook,
+	getNextbookId,
+	getDefaultFilter,
 }
 
 function query(filterBy = {}) {
-    return storageService.query(BOOK_KEY)
-        .then(books => {
-            if (filterBy.title) {
-                const regex = new RegExp(filterBy.title, 'i')
-                books = books.filter(book => regex.test(book.title))
-            }
-            if (filterBy.minPrice) {
-                books = books.filter(book => book.listPrice.amount >= filterBy.minPrice)
-            }
-            return books
-        })
+	return storageService.query(BOOK_KEY).then((books) => {
+		if (filterBy.title) {
+			const regex = new RegExp(filterBy.title, 'i')
+			books = books.filter((book) => regex.test(book.title))
+		}
+		if (filterBy.minPrice) {
+			books = books.filter((book) => book.listPrice.amount >= filterBy.minPrice)
+		}
+		return books
+	})
 }
 
 function get(bookId) {
-    return storageService.get(BOOK_KEY, bookId)
+	return storageService.get(BOOK_KEY, bookId).then(_setNextPrevBookId)
 }
 
 function remove(bookId) {
-    return storageService.remove(BOOK_KEY, bookId)
+	return storageService.remove(BOOK_KEY, bookId)
 }
 
 function save(book) {
-    if (book.id) {
-        return storageService.put(BOOK_KEY, book)
-    } else {
-        return storageService.post(BOOK_KEY, book)
-    }
+	if (book.id) {
+		return storageService.put(BOOK_KEY, book)
+	} else {
+		return storageService.post(BOOK_KEY, book)
+	}
 }
 
 function getNewBook(title = '', author = '') {
-    return { id: '', title, author }
+	return { id: '', title, author }
 }
 
 function getDefaultFilter() {
-    return { title: '', minPrice: undefined }
+	return { title: '', minPrice: undefined }
 }
 
 function getNextbookId(bookId) {
-    return storageService.query(BOOK_KEY)
-        .then(books => {
-            let nextbookIdx = books.findIndex(car => car.id === bookId) + 1
-            if (nextbookIdx === books.length) nextbookIdx = 0
-            return books[nextbookIdx].id
-        })
+	return storageService.query(BOOK_KEY).then((books) => {
+		let nextbookIdx = books.findIndex((car) => car.id === bookId) + 1
+		if (nextbookIdx === books.length) nextbookIdx = 0
+		return books[nextbookIdx].id
+	})
 }
 
 function _createBooks() {
-    let books = loadFromStorage(BOOK_KEY)
-    if (!books || !books.length) {
-        books = getBooks()
-        saveToStorage(BOOK_KEY, books)
-    }
+	let books = loadFromStorage(BOOK_KEY)
+	if (!books || !books.length) {
+		books = getBooks()
+		saveToStorage(BOOK_KEY, books)
+	}
 }
 
 function _createBook(title, author) {
-    const book = getNewBook(title, author)
-    book.id = makeId()
-    return book
+	const book = getNewBook(title, author)
+	book.id = makeId()
+	return book
+}
+
+function _setNextPrevBookId(book) {
+	return query().then((books) => {
+		const bookIdx = books.findIndex((currBook) => currBook.id === book.id)
+		const nextBook = books[bookIdx + 1] ? books[bookIdx + 1] : books[0]
+		const prevBook = books[bookIdx - 1]
+			? books[bookIdx - 1]
+			: books[books.length - 1]
+
+		book.nextBook = nextBook.id
+		book.prevBook = prevBook.id
+		return book
+	})
 }
